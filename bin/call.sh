8b92dc38824c5341dc1cad11b331e635d5a9e639
user2 <user2@no-reply.com>
2021-01-27 05:11:16 +0000

    Update call.sh

--- bin/call.sh
+++ bin/call.sh
@@ -5,7 +5,7 @@ set -o pipefail
 
 KSVC_NAME=${1:-'greeter'}
 
-export $KSVC_HOST=$(oc get rt greeter -o yaml | yq read - 'status.url')
+KSVC_HOST=$(oc get rt greeter -o yaml | yq read - 'status.url')
 
 if [ $# -le 1 ]
 then
