bf45d29275068ac43f96b0561d506d68a5ef2e41
Marc Hildenbrand <mwhildenbrand+github@gmail.com>
2021-01-27 05:09:09 +0000

    Update call.sh for use with CRW

--- bin/call.sh
+++ bin/call.sh
@@ -5,20 +5,7 @@ set -o pipefail
 
 KSVC_NAME=${1:-'greeter'}
 
-CURR_CTX=$(kubectl config current-context)
-
-CURR_NS="$(kubectl config view -o=jsonpath="{.contexts[?(@.name==\"${CURR_CTX}\")].context.namespace}")" \
-    || exit_err "error getting current namespace"
-
-if [[ -z "${CURR_NS}" ]]; 
-then
-  CURR_NS='default'
-else
-  CURR_NS="${CURR_NS}"
-fi
-
-# HOST_HEADER="Host:$KSVC_NAME.$CURR_NS.example.com"
-KSVC_HOST="$KSVC_NAME.$CURR_NS.$(minikube -p knativetutorial ip).nip.io"
+export $KSVC_HOST=$(oc get rt greeter -o yaml | yq read - 'status.url')
 
 if [ $# -le 1 ]
 then
